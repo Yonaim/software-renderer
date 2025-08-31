0c33bed17c7abc753b268d7afc0a1ad4db7e98e4
yonaMac <yonaimyo@gmail.com>
2025-09-01 03:39:55 +0900

    Feat: resource::Add aliases and modify API design

--- include/resource.h
+++ include/resource.h
@@ -2,6 +2,8 @@
 #include "core.h"
 #include "handle.h"
 #include <vector>
+#include <expected>
+#include <map>
 
 namespace resource
 {
@@ -26,21 +28,78 @@ namespace resource
         return "Unknown error";
     }
 #undef X
+    template <class T> using Result = std::expected<T, ErrorCode>;
+
+    enum class OverwritePolicy
+    {
+        Error,
+        KeepExisted,
+        Replace
+    };
+
+    using MeshKey = std::string;
+    using TextureKey = std::string;
+    using MaterialKey = std::pair<std::string, std::string>; // {file, name}
+
+    struct MeshPool
+    {
+        std::vector<core::Mesh>           items;
+        std::map<std::string, MeshHandle> idToHandle;
+    };
+
+    struct MaterialPool
+    {
+        std::vector<core::Material>           items;
+        std::map<MaterialKey, MaterialHandle> idToHandle;
+    };
+
+    struct TexturePool
+    {
+        std::vector<core::Texture>           items;
+        std::map<std::string, TextureHandle> idToHandle;
+    };
+
+    enum class OverwritePolicy
+    {
+        KeepExisted,
+        Replace,
+        Error
+    };
+
+    enum class RegisterOutcome
+    {
+        Reused,
+        Inserted,
+        Replaced
+    };
+
+    template <class HandleT>
+    using RegisterResult = std::expected<std::pair<HandleT, RegisterOutcome>, ErrorCode>;
 
     class Manager
     {
       private:
-        std::vector<core::Mesh>     meshes;
-        std::vector<core::Texture>  textures;
-        std::vector<core::Material> materials;
+        MeshPool     meshes;
+        MaterialPool materials;
+        TexturePool  textures;
 
       public:
-        core::Mesh    &getMesh(MeshHandle handle);
-        core::Texture &getTexture(TextureHandle handle);
-        core::Texture &getMaterial(MaterialHandle handle);
-        uint32_t       addMesh(core::Mesh &m);
-        uint32_t       addTexture(core::Texture &t);
-        uint32_t       addMaterial(core::Material &m);
+        // get
+        core::Mesh     &getMesh(MeshHandle handle) const;
+        core::Material &getMaterial(MaterialHandle handle) const;
+        core::Texture  &getTexture(TextureHandle handle) const;
+
+        RegisterResult<MeshHandle> registerMesh(const MeshKey    &key,
+                                                const core::Mesh *init = nullptr,
+                                                OverwritePolicy pol = OverwritePolicy::KeepExisted);
+
+        RegisterResult<MaterialHandle>
+        registerMaterial(const MaterialKey &key, const core::Material *init = nullptr,
+                         OverwritePolicy pol = OverwritePolicy::KeepExisted);
+
+        RegisterResult<TextureHandle>
+        registerTexture(const TextureKey &key, const core::Texture *init = nullptr,
+                        OverwritePolicy pol = OverwritePolicy::KeepExisted);
     };
 
 } // namespace resource
