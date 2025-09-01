36f91c34e36aa8b386f2694c2304531f7dc9b358
yonaMac <yonaimyo@gmail.com>
2025-09-01 10:10:06 +0900

    Feat: Add HandlesById map

--- include/scene.h
+++ include/scene.h
@@ -4,18 +4,18 @@
 #include <optional>
 #include <variant>
 #include <vector>
+#include <unordered_map>
 
 // TODO: 기본 값 설정
 namespace scene
 {
     struct Object
     {
-        MeshHandle                   mesh;
-        MaterialHandle               mat;
-        std::optional<TextureHandle> tex;
-        math::Vec3                   pos;
-        math::Vec3                   rot;
-        math::Vec3                   scale;
+        std::string id;
+        MeshHandle  mesh;
+        math::Vec3  pos;
+        math::Vec3  rot;
+        math::Vec3  scale;
     };
 
     struct Camera
@@ -25,14 +25,19 @@ namespace scene
         float      fovY;
         float      znear;
         float      zfar;
+
+        Camera() = default;
+        Camera(const math::Vec3 &p, const math::Vec3 &r, float fovy, float zn, float zf) noexcept
+            : pos(p), rot(r), fovY(fovy), znear(zn), zfar(zf);
     };
 
     namespace light
     {
         struct Common
         {
-            math::Vec3 color;
-            float      intensity;
+            std::string id;
+            math::Vec3  color;
+            float       intensity;
         };
 
         struct Directional : Common
@@ -60,17 +65,26 @@ namespace scene
         };
     } // namespace light
 
-    using Light = std::variant<light::Directional, light::Point, light::Spot,
-                               light::Ambient>;
+    using Light = std::variant<light::Directional, light::Point, light::Spot, light::Ambient>;
 
     struct Scene
     {
-        std::vector<Object> objects;
-        std::vector<Light>  lights;
+        std::string         name;
         Camera              camera;
-        void                addObject(const Object &obj);
-        void                addLight(const Light &light);
-        void                setCamera(const Camera &cam);
+        std::vector<Light>  lights;
+        std::vector<Object> objects;
+
+        struct HandlesById // id -> handle
+        {
+            std::unordered_map<std::string, MeshHandle>     mesh;
+            std::unordered_map<std::string, MaterialHandle> material;
+            std::unordered_map<std::string, TextureHandle>  texture;
+        };
+
+        Scene() = default;
+        void addObject(const Object &obj);
+        void addLight(const Light &light);
+        void setCamera(const Camera &cam);
     };
 
 } // namespace scene
